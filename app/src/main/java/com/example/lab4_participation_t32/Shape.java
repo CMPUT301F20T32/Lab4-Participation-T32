c636713076b3268f9deefa7352f4695b818367f3
Vyome Agarwal <vyome@ualberta.ca>
2020-10-04 17:01:19 -0600

    Created Shape.java

---
+++ app/src/main/java/com/example/lab4_participation_t32/Shape.java
@@ -0,0 +1,13 @@
+package com.example.lab4_participation_t32;
+
+public abstract class Shape {
+
+    private int x;
+    private int y;
+
+    public Shape(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+}
